6ea2b0e4950ea880a68c1e504fc1422e8d5daebd
Fernando <fernangd9@gmail.com>
2021-09-16 00:14:52 +0200

    Initialized Spring Boot app

---
+++ src/main/java/com/fgiron/votosAuthServer/VotosAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.fgiron.votosAuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fgiron/votosAuthServer/VotosAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgiron.votosAuthServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
